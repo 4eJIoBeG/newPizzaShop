1465cf2f6dbe005b3f2e02738dff537dba0829d4
4eJIoBeG <pavelshashenko@gmail.com>
2024-03-06 21:38:31 +0300

    add routes different way

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
 import Menu from "./pages/Menu/Menu";
 import Cart from "./pages/Cart/Cart";
+import Error from "./pages/Error/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
 
 function App() {
   return (
@@ -16,10 +32,7 @@ function App() {
         <a href="/">Меню</a>
         <a href="/cart">Корзина</a>
       </div>
-      <Routes>
-        <Route path="/" element={<Menu />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+      <RouterProvider router={router} />
     </>
   );
 }
