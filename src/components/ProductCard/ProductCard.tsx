d20fa846a8352534403e879c577043371de8d97b
4eJIoBeG <pavelshashenko@gmail.com>
2024-03-07 17:20:30 +0300

    add productcard

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import styles from "./ProductCard.module.css";
+import { ProductCardProps } from "./ProductCard.props";
+
+const ProductCard = (props: ProductCardProps) => {
+  const { id, title, price, description, rating, image } = props;
+  return (
+    <Link to={`/product/${id}`} className={styles["link"]}>
+      <div className={styles["card"]}>
+        <div
+          className={styles["header"]}
+          style={{ backgroundImage: `url('${image}')` }}
+        >
+          <div className={styles["price"]}>
+            {price}&nbsp;
+            <span className={styles["currency"]}>₽</span>
+          </div>
+          <button className={styles["add-to-cart"]}>
+            <img src="/shop.svg" alt="add-to-cart-icon" />
+          </button>
+          <div className={styles["rating"]}>
+            {rating}&nbsp;
+            <img src="/star.svg" alt="star" />
+          </div>
+        </div>
+        <div className={styles["info"]}>
+          <div className={styles["title"]}>{title}</div>
+          <div className={styles["description"]}>{description}</div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductCard;
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,13 +1,26 @@
 import Header from "../../components/Header/Header";
+import ProductCard from "../../components/ProductCard/ProductCard";
 import Search from "../../components/Search/Search";
 import styles from "./Menu.module.css";
 
 const Menu = () => {
   return (
-    <div className={styles["head"]}>
-      <Header>Меню</Header>
-      <Search />
-    </div>
+    <>
+      <div className={styles["head"]}>
+        <Header>Меню</Header>
+        <Search />
+      </div>
+      <div>
+        <ProductCard
+          id={1}
+          title="Наслаждение"
+          description="Салями, руккола, помидоры, оливки"
+          price={300}
+          rating={4.5}
+          image="/img.jpg"
+        />
+      </div>
+    </>
   );
 };
 
